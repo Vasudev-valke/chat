73e87cc860dc963883effa3e65473f54e7cdb672
vasudev valke <vasudev.valke@gmail.com>
2020-01-07 15:13:48 +0530

    chatapp

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ApplicationRecord
+
+validates :title, presence: true, length: {minimum: 5, maximum: 50}
+validates :description, presence: true, length: {minimum: 10, maximum: 100}
+belongs_to :user
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :articles
+
+  #get 'signup', to: 'users#new'
+  resources :users
+
+  resources :sessions, only: [:new, :create]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+delete 'logout', to: 'sessions#destroy'
+end
